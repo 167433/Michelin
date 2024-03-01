f51cf38c6df095aa9af7480f01d749d52b4f3120
167433 <100282502+167433@users.noreply.github.com>
2024-03-01 12:47:28 +0100

    Prosty CRUD

---
+++ MovieLibary/movieLibary/src/main/java/pl/javaprojektpierwszy/movieLibary/MovieLibaryApplication.java
@@ -0,0 +1,14 @@
+package pl.javaprojektpierwszy.movieLibary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieLibaryApplication {
+	public static void main(String[] args) {
+
+		SpringApplication.run(MovieLibaryApplication.class, args);
+
+	}
+
+}
